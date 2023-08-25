96ad0b7bdaf74df1c91a999b27065df335dd32b7
Angelica Turbela <angelcturbela@gmail.com>
2023-08-25 18:29:07 +0200

    createworkshop

--- src/Pages/CreateWorkshopPage.jsx
+++ src/Pages/CreateWorkshopPage.jsx
@@ -34,12 +34,16 @@ function CreateWorkshopPage() {
       teacherId,
       sessionsAvailable,
     };
-    axios .post(`${API_URL}/api/create-workshop`, workshopToCreate)
-    .then((response) => {
-    })
-    .catch((error) => {
+    axios
+      .post(`${API_URL}/api/create-workshop`, workshopToCreate)
+      .then((response) => {
+        console.log(response);
+        const newWorkshop = response.data;
+        navigate("/see-workshops");
+      })
+      .catch((error) => {
         console.log(error);
-    })
+      });
   };
 
   return (
@@ -58,6 +62,67 @@ function CreateWorkshopPage() {
         <option value="Baking">Baking</option>
         <option value="Patisserie">Patisserie</option>
       </select>
+      <label>Sub Category</label>
+      <select
+        name="subCategory"
+        onChange={(e) => setSubCategory(e.target.value)}
+      >
+        <option value="">Choose an option</option>
+        <option value="Japanese Cuisine">Japanese Cuisine</option>
+        <option value="French Cuisine">French Cuisine</option>
+        <option value="South American Cuisine">South American Cuisine</option>
+        <option value="Chinese Cuisine">Chinese Cuisine</option>
+        <option value="Italian Cuisine">Italian Cuisine</option>
+      </select>
+      <label>Duration</label>
+      <select name="duration" onChange={(e) => setDuration(e.target.value)}>
+        <option value="">Choose an option</option>
+        <option value="1h">1h</option>
+        <option value="2h">2h</option>
+        <option value="3h">3h</option>
+      </select>
+      <label>Max Participants</label>
+      <input
+        type="number"
+        name="maxParticipants"
+        value={maxParticipants}
+        onChange={(e) => setMaxParticipants(e.target.value)}
+      ></input>
+      <label>Workshop Photos</label>
+      <input
+        type="image"
+        name="workshopPics"
+        value={workshopPics}
+        onChange={(e) => setWorkshopPics(e.target.value)}
+      ></input>
+      <label>Location</label>
+      <input
+        type="text"
+        name="location"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      ></input>
+      <label>Workshop Materials</label>
+      <input
+        type="text"
+        name="workshopMaterial"
+        value={workshopMaterial}
+        onChange={(e) => setWorkshopMaterial(e.target.value)}
+      ></input>
+      <label>Price</label>
+      <input
+        type="number"
+        name="price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      ></input>
+      <label>Sessions Available</label>
+      <input
+        type="date"
+        name="date"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      ></input>
 
       <button onClick={handleSubmit}> Submit </button>
     </div>
