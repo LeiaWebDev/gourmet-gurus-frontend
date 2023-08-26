97af357ccb7ffac250e73a7d2065af40ad1921d7
Angelica Turbela <angelcturbela@gmail.com>
2023-08-26 19:19:08 +0200

    css

--- src/Pages/CreateWorkshopPage.jsx
+++ src/Pages/CreateWorkshopPage.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/AuthContext";
-
+import './createWorkshop.css'
 const API_URL = import.meta.env.VITE_API_URL;
 
 function CreateWorkshopPage() {
