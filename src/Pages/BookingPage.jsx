f21899036e46a80a2997129ea1ffb975aaf28f4b
LeiaWebDev <leia@cowmeuh.net>
2023-08-28 15:37:41 +0200

    booking page fixed bugs

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -7,19 +7,22 @@ import IsLoggedIn from "../Components/IsLoggedIn";
 const API_URL = import.meta.env.VITE_API_URL;
 
 function BookingPage() {
-  const [user, setUser] = useState("")
+  const [user, SetUser] =useState(null)
+  // const [isLoggedIn, setIsLoggedIn] = useState("true")
   const [booking, setBooking] = useState(null)
   const [loading, setLoading] = useState(true)
   const {bookingId} = useParams()
-  const [sessionAvailable, setSessionsAvailable] = useState("")
+  const [sessionsAvailable, setSessionsAvailable] = useState("")
   const [quantity, setQuantity] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0)
+  const navigate = useNavigate()
   // const [counter, setCounter] = useState(1);
   // let incrementCounter = () => setCounter(counter + 1);
   // let decrementCounter = () => setCounter(counter - 1);
   // if(counter<=0) {
   //   decrementCounter = () => setCounter(1);
   // }
-// const totalAmount = workshop.price * counter
+  
 
   // const setQuantity = (amount) => {
   //   // const setQuantity = (workshop, amount) => {
@@ -27,33 +30,45 @@ function BookingPage() {
   //   // ...item,
   //   // quantity: item.quantity + amount
   //   // }:item))
-  //   setBooking((prevBooking) => ({
-  //     ...prevBooking,
-  //     quantity: prevBooking.quantity + amount
-  //   }));
-  // };
+  
 
     // workshop.map((sessions, index)=>{
     //   workshop.sessionsAvailable.index
     // })
 
     const handleIncrement = () =>{
-      setQuantity(()=>(booking.quantity += 1))
-    }  
+      // setQuantity(()=>(booking.quantity += 1))
+      setBooking((prevBooking) => ({
+      ...prevBooking,
+      quantity: prevBooking.quantity + 1
+      }));
+    };
+    
     const handleDecrement = () => {
       if (booking.quantity>0){
-        setQuantity(()=>(booking.quantity -= 1))
+        setBooking((prevBooking) => ({
+          ...prevBooking,
+          quantity: prevBooking.quantity - 1
+        }));
+        // setQuantity(()=>(booking.quantity -= 1))
       }
-      
+
     }
-  const handleBooking=()=>{
-    if(user||workshopId){
-      navigate(`/booking-details`)
-    } else {
-        alert ("You must be logged in to book a workshop")
-        navigate(`/login`)
+
+    const calculateTotalPrice=()=>{
+      return booking.quantity * booking.workshopId.price
     }
-  }
+
+
+    const handleBooking=()=>{
+      // if(user||workshopId){
+      if(user||booking.workshopId){
+        navigate(`/booking-details`)
+      } else {
+          alert ("You must be logged in to book a workshop")
+          navigate(`/login`)
+      }
+    }
 
   useEffect(() => {
     const getBookingDetails = async ()=>{
@@ -91,44 +106,45 @@ function BookingPage() {
   // }, [booking]);
 
 
+  
+
   if(!booking){
     return <div>Loading...</div>
   }
 
+
   return (
     <div>
-      <h1>Booking Details</h1>
+      <h1>Your Booking Details</h1>
       <h2>{booking.workshopId.title}</h2>
       <img
             src={booking.workshopId.workshopPics[0]}
             alt={booking.workshopId.title}
       />
-      <p>{booking.workshopId.duration}</p>
+      <p>{booking.workshopId.duration} workshop</p>
       
-      <p>{booking.workshopId.location}</p>
-      <p>{booking.workshopId.price}$/pers</p>
+      <p>Address : {booking.workshopId.location}</p>
+      <p>{booking.workshopId.price} € / pers</p>
+
       <div className='quantity'> 
-        <p> Quantity: {booking.quantity} </p>
-        {/* <button className='buttonCart' onClick={(e)=> setQuantity(1)}> */}
-        <button className='buttonCart' onClick={(e)=> handleIncrement()}>
-          +
-        </button>
-        {/* <button className='buttonCart' onClick={(e)=> setQuantity(-1)}> */}
+        <p> Quantity:  </p>
         <button className='buttonCart' onClick={(e)=> handleDecrement()}>
           -
         </button>
-        {/* <ButtonIncrement onClick={incrementCounter}/>
-        <Display message={counter}/> 
-        <ButtonDecrement onClick={decrementCounter}/> */}
+        <p>{booking.quantity}</p>
+        <button className='buttonCart' onClick={(e)=> handleIncrement()}>
+          +
+        </button>
       </div>
-
+      <p>Total Price : {calculateTotalPrice()} euros</p>
       <div>
         <h3>Choose your session</h3>
         
-          <select name="sessionsAvailable"  value={sessionAvailable} onChange={(e) => setSessionsAvailable(e.target.value)}>
+          <select name="sessionsAvailable"  value={sessionsAvailable} onChange={(e) => setSessionsAvailable(e.target.value)}>
             <option value="-1" disabled>Choose an option</option>
             {booking.workshopId.sessionsAvailable.map(date => {
-              return <option value={date}>{new Date(date).toLocaleDateString()}</option>
+              console.log(date)
+              return <option key={booking.workshopId.sessionsAvailable.date} value={date}>{new Date(date).toLocaleDateString()}</option>
 
             })}
 
